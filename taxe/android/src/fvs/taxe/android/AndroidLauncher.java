07e1b707c760048576e77dabc9de852b0a73eca2
Owen Hurford <owen@hurfo.com>
2015-04-06 12:02:45 +0100

    Android project

---
+++ taxe/android/src/fvs/taxe/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package fvs.taxe.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import fvs.taxe.TaxeGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TaxeGame(), config);
+	}
+}
