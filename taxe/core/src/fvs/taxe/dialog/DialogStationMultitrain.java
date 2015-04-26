369e7e75b8fbe4cfdcffaa4d3f4a9ce316558a3a
James Judd <juddling@assassinators.net>
2015-04-26 15:21:19 +0100

    fix for https://github.com/SEPRFVS/FVS4/issues/13

--- taxe/core/src/fvs/taxe/dialog/DialogStationMultitrain.java
+++ taxe/core/src/fvs/taxe/dialog/DialogStationMultitrain.java
@@ -3,6 +3,8 @@ package fvs.taxe.dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import fvs.taxe.clickListener.TrainClicked;
 import fvs.taxe.controller.Context;
+import gameLogic.player.Player;
+import gameLogic.resource.Resource;
 import gameLogic.resource.Train;
 
 import java.util.ArrayList;
@@ -24,11 +26,38 @@ public class DialogStationMultitrain extends ReplayDialog {
             if (train.getFinalDestination() != null) {
                 destination = " to " + train.getFinalDestination().getName();
             }
-            button(train.getName() + destination + " (Player " + train.getPlayer().getPlayerNumber() + ")", train);
+
+
+            button(train.getName() + destination + " (Player " + train.getPlayer().getPlayerNumber() + ")", trainString(train));
             getButtonTable().row();
         }
 
         button("Cancel", "CANCEL");
+    }
+
+    private String trainString(Train train) {
+        return train.getName() + ":" + train.getPlayer().getPlayerNumber() + ":" + train.getPosition().getX()
+                + ":" + train.getPosition().getY();
+    }
+
+    private Train getTrainFromString(String s) {
+//        String[] parts = s.split(":");
+//        int playerNumber = Integer.parseInt(parts[1]);
+
+        for (Player player : context.getGameLogic().getPlayerManager().getAllPlayers()) {
+            for (Resource resource : player.getResources()) {
+                if (!(resource instanceof Train)) continue;
+
+                Train train = (Train) resource;
+
+                String trainString = trainString(train);
+
+                if (trainString.equals(s))
+                    return train;
+            }
+        }
+
+        throw new RuntimeException("Train couldn't be found");
     }
 
     @Override
@@ -39,8 +68,10 @@ public class DialogStationMultitrain extends ReplayDialog {
             //If the user clicks cancel then it deletes the dialog
             this.remove();
         } else {
+            Train train = getTrainFromString((String) obj);
+
             //Simulate click on the train
-            TrainClicked clicker = new TrainClicked(context, (Train) obj, null);
+            TrainClicked clicker = new TrainClicked(context, train, null);
             //This is a small hack, by setting the value of the simulated x value to -1, we can use this to check whether or not
             //This dialog has been opened before. If this was not here then this dialog and trainClicked would get stuck in an endless loop!
             clicker.clicked(null, -1, 0);
