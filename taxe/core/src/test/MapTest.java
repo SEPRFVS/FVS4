1054014f905aaa93667ece9d099d76b0b10c1117
Owen Hurford <owen@hurfo.com>
2015-04-28 19:36:27 +0100

    Two more Unit tests

--- taxe/core/src/test/MapTest.java
+++ taxe/core/src/test/MapTest.java
@@ -33,4 +33,25 @@ public class MapTest extends LibGdxTest {
         // Should throw an error by itself
         map.getStationFromPosition(new Position(9999, 9999));
     }
+    
+    @Test
+    public void dijkstraUpdateTest() {
+    	map.addStation("station1", new Position(100, 100));
+    	map.addStation("station2", new Position(200, 200));
+    	map.addStation("station3", new Position(200, 100));
+    	
+    	map.addConnection("station1", "station3");
+    	map.addConnection("station3", "station2");
+    	
+    	map.updateDijkstra();
+    	
+    	assertTrue(map.getShortestDistance(map.getStationByName("station1"), map.getStationByName("station2")) == 200);
+    	
+    	map.addConnection("station1", "station2");
+    	
+    	map.updateDijkstra();
+    	
+    	//Use int as game works based on pixels so decimals not needed
+    	assertTrue((int) map.getShortestDistance(map.getStationByName("station1"), map.getStationByName("station2")) == (int) Math.sqrt(Math.pow(100, 2) * 2));
+    }
 }
--- taxe/core/src/test/TrainTest.java
+++ taxe/core/src/test/TrainTest.java
@@ -4,25 +4,32 @@ package test;
 import gameLogic.map.Position;
 import gameLogic.map.Station;
 import gameLogic.resource.Train;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TrainTest {
     Train train;
+    Station station1;
+    Station station2;
+    ArrayList<Station> route;
 
-    @Before
+	@Before
     public void trainSetup() throws Exception {
-        train = new Train("RedTrain", "RedTrain.png", 250);
+        train = new Train("RedTrain", "NuclearTrain.png", 250);
+        station1 = new Station("station1", new Position(5, 5));
+        station2 = new Station("station2", new Position(6, 6));
+        route = new ArrayList<Station>();
+        route.add(station1);
+        route.add(station2);
     }
 
     @Test
     public void finalDestinationTest() throws Error {
-        Station station1 = new Station("station1", new Position(5, 5));
-        Station station2 = new Station("station2", new Position(6, 6));
         ArrayList<Station> route = new ArrayList<Station>();
         route.add(station1);
         route.add(station2);
@@ -30,6 +37,14 @@ public class TrainTest {
         train.setRoute(route);
         assertTrue("Setting a train route was not succesful", train.getRoute().size() == 2);
         assertTrue("Final destination wasn't set", train.getFinalDestination() == station2);
+    }
+    
+    @Test
+    public void rotationTest() {
+    	train.setRoute(route);
+    	train.addHistory(station1, 0);
+    	double angle = 360.0f - Math.toDegrees(Math.atan2(train.getNextStation().getLocation().getX() - train.getLastStation().getLocation().getX(), train.getNextStation().getLocation().getY() - train.getLastStation().getLocation().getY()));
+    	assertTrue(315.0f == (float) angle);
     }
 
 
